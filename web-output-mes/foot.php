228b54d30bf74a0fa3bd8248391c61bb57094cc4
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2022-10-26 22:52:51 +0800

    Delete web-output-mes directory

--- web-output-mes/foot.php
+++
@@ -1,5 +0,0 @@
-<div style="text-align:left;">
-<span>页面加载时长：{number_format((microtime(true) - $_SERVER['_start_time']), 3)} 秒</span>
-<span>数据库查询：{$_SERVER['_query_count']} 次</span>
-<span>占用内存：{round((memory_get_usage() - $_SERVER['_memory_usage'])/1024/1024,2)} MB</span>
-</div>
